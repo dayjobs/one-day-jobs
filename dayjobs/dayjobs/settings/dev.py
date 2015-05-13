ca676750726e1d6c9695ae2035b1e9d42228529a
Ryan Wong <ryan.wong022@gmail.com>
2015-05-13 12:27:47 +0800

    fab

---
+++ dayjobs/dayjobs/settings/dev.py
@@ -0,0 +1,38 @@
+from dayjobs.settings.common import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = True
+
+ALLOWED_HOSTS = ['127.0.0.1:8000']
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'dayjobs',
+        'USER': 'dayjobs',
+        'PASSWORD': 'dayjobs',
+        'HOST' : 'localhost',
+        'PORT' : '5432',
+    }
+}
+
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR, 'templates'),
+)
+
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+#MEDIA FILE (user uploaded files)
+MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
+MEDIA_URL = '/media/'
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_ENABLED = os.environ.get('COMPRESS_ENABLED', False)
